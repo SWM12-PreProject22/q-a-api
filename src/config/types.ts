7a861c7c34603a2145613e5fbe48bcd986149ba6
SeungWon <20sunrin041@sunrint.hs.kr>
2021-04-22 02:01:27 +0900

    feat: Add graphql schema

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,8 +1,21 @@
 import { ReadStream } from "fs"
-
+import { ObjectID } from "mongodb"
 export interface File {
     filename: string
     mimetype: string
     encoding: string
     createReadStream: () => ReadStream
+}
+
+export interface QNA {
+    id: string
+    qnaId: ObjectID
+    content: string
+}
+
+export interface Comment {
+    id: string
+    qndId: ObjectID
+    contentId: ObjectID
+    content: string
 }
