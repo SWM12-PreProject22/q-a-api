58225a9f29e4b031713252762544e722e7c015df
SeungWon <20sunrin041@sunrint.hs.kr>
2021-04-22 10:20:31 +0900

    refactor: Delete File interface & Update QNA interface

--- src/config/types.ts
+++ src/config/types.ts
@@ -1,4 +1,3 @@
-import { ReadStream } from "fs"
 import { ObjectID } from "mongodb"
 import DataLoader from "dataloader"
 
@@ -6,17 +5,12 @@ export interface Loaders {
     commentsLoader: DataLoader<ObjectID, any, ObjectID>
 }
 
-export interface File {
-    filename: string
-    mimetype: string
-    encoding: string
-    createReadStream: () => ReadStream
-}
-
 export interface QNA {
     id: string
     _id: ObjectID
     content: string
+    status: Boolean
+    comment: Comment[]
 }
 
 export interface Comment {
