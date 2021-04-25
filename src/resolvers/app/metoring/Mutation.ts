673280fc7054701d1eea57fcbb46d53bbd16c490
SeungWon <20sunrin041@sunrint.hs.kr>
2021-04-26 00:46:35 +0900

    chore: Error handling

--- src/resolvers/app/metoring/Mutation.ts
+++ src/resolvers/app/metoring/Mutation.ts
@@ -1,2 +1,14 @@
 import { ApolloError } from "apollo-server-errors"
 import { ObjectId, Db } from "mongodb"
+
+export default {
+    getAllTopic: async (parent: void, args: void, { db }: { db: Db }) => await db.collection("topic").find({ status: true }).toArray(),
+    getTopicById: async (parent: void, { id }: { id: string }, { db }: { db: Db }) => {
+        try {
+            const _id = new ObjectId(id)
+            return await db.collection("topic").findOne({ _id })
+        } catch {
+            throw new ApolloError("id가 ObjectId가 아닙니다.")
+        }
+    }
+}
