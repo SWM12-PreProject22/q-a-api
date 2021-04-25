317cdb30797711eecb3918b6644adca5ac7015fc
SeungWon <20sunrin041@sunrint.hs.kr>
2021-04-26 00:44:08 +0900

    feat: Add getTopicById query

--- src/resolvers/app/metoring/Query.ts
+++ src/resolvers/app/metoring/Query.ts
@@ -2,6 +2,6 @@ import { ApolloError } from "apollo-server-errors"
 import { ObjectId, Db } from "mongodb"
 
 export default {
-    getAllTopic: async (parent: void, args: void, { db }: { db: Db }) => await db.collection("topic").find({ status: true }).toArray()
-
+    getAllTopic: async (parent: void, args: void, { db }: { db: Db }) => await db.collection("topic").find({ status: true }).toArray(),
+    getTopicById: async (parent: void, { id }: { id: string }, { db }: { db: Db }) => await db.collection("topic").findOne({ _id: id })
 }
