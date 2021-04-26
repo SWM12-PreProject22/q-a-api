2d4bce7014046d951eb8f8460ebbddb71f4fe8c0
SeungWon <20sunrin041@sunrint.hs.kr>
2021-04-27 02:01:22 +0900

    feat: Add getIdByQNA query

--- src/resolvers/app/qna/Query.ts
+++ src/resolvers/app/qna/Query.ts
@@ -1,4 +1,5 @@
-import { Db } from "mongodb"
+import { Db, ObjectId } from "mongodb"
+import { ApolloError } from "apollo-server-errors"
 
 export default {
     test: () => "Server On",
@@ -6,5 +7,13 @@ export default {
     getAllQNA: async (parent: void, args: void, { db }: { db: Db }) => await db.collection("post").find({}).toArray(),
     getCloseQNA: async (parent: void, args: void, { db }: { db: Db }) => await db.collection("post").find({ status: false }).toArray(),
     getMyQNA: async (parent: void, { id }: { id: string }, { db }: { db: Db }) => await db.collection("post").find({ id }).toArray(),
-    getDateByQNA: async (parent: void, { date }: { date: number }, { db }: { db: Db }) => await db.collection("post").find({ date: { $gte: date } }).toArray()
+    getDateByQNA: async (parent: void, { date }: { date: number }, { db }: { db: Db }) => await db.collection("post").find({ date: { $gte: date } }).toArray(),
+    getIdByQNA: async (parent: void, { qnaId }: { qnaId: string }, { db }: { db: Db }) => {
+        try {
+            const _id = new ObjectId(qnaId)
+            return await db.collection("post").findOne({ _id })
+        } catch {
+            throw new ApolloError("qnaId가 ObjectId가 아닙니다.")
+        }
+    }
 }
